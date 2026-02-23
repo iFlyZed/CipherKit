853f682636d932ee4c133d737cd7a442d0026b6b
Kirill Zaitsev <iflyzed@icloud.com>
2026-02-24 00:02:54 +0500

    Initial Commit

---
+++ CipherKit/CipherKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  CipherKitApp.swift
+//  CipherKit
+//
+//  Created by Кирилл Зайцев on 24.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct CipherKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CipherKit/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CipherKit
+//
+//  Created by Кирилл Зайцев on 24.02.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
